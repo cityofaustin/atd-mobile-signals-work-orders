f08b7b32f406d22b62ae387f6fe3f3609cb63c8c
Mateo Clarke <code@mateoclarke.com>
2018-09-25 22:04:41 -0500

    display data as within list items

--- src/Home.js
+++ src/Home.js
@@ -12,13 +12,35 @@ class Home extends Component {
   }
 
   render() {
+    // make sure the data is not an empty object `{}`
+    const markingsData =
+      this.props.knackData.length > 0 ? this.props.knackData : [];
+    console.log("markingsData", markingsData);
+
     return (
       <div>
         <h1>
           <FontAwesomeIcon icon={faRoad} /> Markings Work Orders
         </h1>
 
-        <code>{JSON.stringify(this.props.knackData)}</code>
+        <ul className="list-group">
+          {markingsData.map(item => (
+            <li className="list-group-item d-flex row" key={item.id}>
+              {/* Location */}
+              <div className="col-12">
+                <span>{item.field_2287}</span>
+              </div>
+              {/* Status */}
+              <div className="col-6">
+                <span>{item.field_2181}</span>
+              </div>
+              {/* Created Date */}
+              <div className="col-6">
+                <span>{item.field_2148}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }

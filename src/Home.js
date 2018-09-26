1ea471957abe00924af986497e27bc505878c0d1
Mateo Clarke <code@mateoclarke.com>
2018-09-25 23:39:42 -0500

    add styling and icons by status

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,49 @@
 import React, { Component } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRoad } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRoad,
+  faMapMarkerAlt,
+  faClock,
+  faFlagCheckered,
+  faCheckCircle,
+  faTimes,
+  faTruck,
+  faExclamationTriangle
+} from "@fortawesome/free-solid-svg-icons";
+
+const statusMap = {
+  "ON HOLD": {
+    icon: faClock,
+    backgroundColor: "#aeaeae",
+    textColor: "white"
+  },
+  "FINAL REVIEW": {
+    icon: faFlagCheckered,
+    backgroundColor: "#4daf4a",
+    textColor: "white"
+  },
+  "NEED TO BE ISSUED": {
+    icon: faExclamationTriangle,
+    backgroundColor: "#e41a1c",
+    textColor: "white"
+  },
+  ISSUED: {
+    icon: faTruck,
+    backgroundColor: "#377eb8",
+    textColor: "white"
+  },
+  CLOSED: {
+    icon: faCheckCircle,
+    backgroundColor: "white",
+    textColor: "black"
+  },
+  CANCELLED: {
+    icon: faTimes,
+    backgroundColor: "white",
+    textColor: "black"
+  }
+};
 
 class Home extends Component {
   componentDidMount() {
@@ -13,9 +55,8 @@ class Home extends Component {
 
   render() {
     // make sure the data is not an empty object `{}`
-    const markingsData =
-      this.props.knackData.length > 0 ? this.props.knackData : [];
-    console.log("markingsData", markingsData);
+    const isMarkingsDataLoaded = this.props.knackData.length > 0;
+    const markingsData = isMarkingsDataLoaded ? this.props.knackData : [];
 
     return (
       <div>
@@ -24,22 +65,34 @@ class Home extends Component {
         </h1>
 
         <ul className="list-group">
-          {markingsData.map(item => (
-            <li className="list-group-item d-flex row" key={item.id}>
-              {/* Location */}
-              <div className="col-12">
-                <span>{item.field_2287}</span>
-              </div>
-              {/* Status */}
-              <div className="col-6">
-                <span>{item.field_2181}</span>
-              </div>
-              {/* Created Date */}
-              <div className="col-6">
-                <span>{item.field_2148}</span>
-              </div>
-            </li>
-          ))}
+          {isMarkingsDataLoaded &&
+            markingsData.map(item => (
+              <li
+                className="list-group-item d-flex row"
+                key={item.id}
+                style={{
+                  backgroundColor: statusMap[item.field_2181].backgroundColor,
+                  color: statusMap[item.field_2181].textColor
+                }}
+              >
+                {/* Location */}
+                <div className="col-12">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} />
+                  <span>{item.field_2287}</span>
+                </div>
+                {/* Status */}
+                <div className="col-6">
+                  <FontAwesomeIcon
+                    icon={item.field_2181 && statusMap[item.field_2181].icon}
+                  />
+                  <span> {item.field_2181}</span>
+                </div>
+                {/* Created Date */}
+                <div className="col-6">
+                  <span>{item.field_2148}</span>
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
     );

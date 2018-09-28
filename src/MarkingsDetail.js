c50ff060f9a82c54bcf76f1857b54b00dfcbf407
Mateo Clarke <code@mateoclarke.com>
2018-09-28 17:50:00 -0500

    finish out work order details display with loading spinners

--- src/MarkingsDetail.js
+++ src/MarkingsDetail.js
@@ -6,7 +6,8 @@ import {
   faWrench,
   faInfoCircle,
   faClipboard,
-  faPaperclip
+  faPaperclip,
+  faSpinner
 } from "@fortawesome/free-solid-svg-icons";
 
 import {
@@ -17,7 +18,7 @@ import {
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
 
-const fields = [
+const detailsFields = [
   { "Street Segment ID": "field_2591" },
   { Status: "field_2181" },
   { "Work Groups": "field_2203" },
@@ -39,29 +40,80 @@ class MarkingsDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markingDetailData: []
+      markingDetailData: false,
+      commentsData: false,
+      jobsData: false,
+      attachmentsData: false
+    };
+    this.requestOptions = {
+      headers: {
+        "X-Knack-Application-Id": "5b633d68c04cc40730078ac3",
+        "X-Knack-REST-API-KEY": "knack",
+        Authorization: this.props.knackUserToken,
+        "content-type": "application/json"
+      }
     };
   }
+
   componentDidMount() {
+    this.requestMarkingDetailsData();
+  }
+
+  requestMarkingDetailsData = () => {
     axios
       .get(
         `https://us-api.knack.com/v1/scenes/scene_713/views/view_1885/records/${
           this.props.markingId
         }`,
-        {
-          headers: {
-            "X-Knack-Application-Id": "5b633d68c04cc40730078ac3",
-            "X-Knack-REST-API-KEY": "knack",
-            Authorization: this.props.knackUserToken,
-            "content-type": "application/json"
-          }
-        }
+        this.requestOptions
       )
       .then(res => {
         console.log(res);
         this.setState({ markingDetailData: res.data });
       });
-  }
+  };
+
+  requestCommentsData = () => {
+    axios
+      .get(
+        `https://us-api.knack.com/v1/scenes/scene_713/views/view_1953/records?view-work-orders-marking-details_id=/${
+          this.props.markingId
+        }`,
+        this.requestOptions
+      )
+      .then(res => {
+        console.log("commentsData", res);
+        this.setState({ commentsData: res.data.records });
+      });
+  };
+
+  requestJobsData = () => {
+    axios
+      .get(
+        `https://us-api.knack.com/v1/scenes/scene_713/views/view_2158/records?view-work-orders-marking-details_id=${
+          this.props.markingId
+        }`,
+        this.requestOptions
+      )
+      .then(res => {
+        console.log("jobsData", res);
+        this.setState({ jobsData: res.data.records });
+      });
+  };
+
+  requestAttachmentsData = () => {
+    axios
+      .get(
+        `https://us-api.knack.com/v1/scenes/scene_713/views/view_1975/records?view-work-orders-marking-details_id=${
+          this.props.markingId
+        }`,
+        this.requestOptions
+      )
+      .then(res => {
+        console.log("attachmentsData", res);
+        this.setState({ attachmentsData: res.data.records });
+      });
+  };
 
   render() {
     return (
@@ -73,12 +125,13 @@ class MarkingsDetail extends Component {
         <Accordion>
           <AccordionItem>
             <AccordionItemTitle>
-              <h3>
+              <h3 className="u-position-relative">
                 <FontAwesomeIcon icon={faInfoCircle} /> Marking Details
+                <div className="accordion__arrow" role="presentation" />
               </h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              {fields.map(field => (
+              {detailsFields.map(field => (
                 <dl>
                   <dt>{Object.keys(field)[0]}</dt>
                   <dd>
@@ -88,35 +141,105 @@ class MarkingsDetail extends Component {
               ))}
             </AccordionItemBody>
           </AccordionItem>
-          <AccordionItem>
+          <AccordionItem onClick={this.requestCommentsData}>
             <AccordionItemTitle>
-              <h3>
+              <h3 className="u-position-relative">
                 <FontAwesomeIcon icon={faComments} /> Comments
+                <div className="accordion__arrow" role="presentation" />
               </h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              <p>Body content</p>
+              {this.state.commentsData.length === 0 && <p>No data</p>}
+              {this.state.commentsData.length > 0 && (
+                <ul className="list-group list-group-flush">
+                  {this.state.commentsData.map(comment => (
+                    <li className="list-group-item d-flex row">
+                      <div className="col-12">{comment.field_2192}</div>
+                      <div className="col-6">{comment.field_2193}</div>
+                      <div
+                        className="col-6"
+                        dangerouslySetInnerHTML={{ __html: comment.field_2194 }}
+                      >
+                        {}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {!this.state.commentsData && (
+                <div>
+                  <FontAwesomeIcon
+                    icon={faSpinner}
+                    size="2x"
+                    className="atd-spinner"
+                  />
+                </div>
+              )}
             </AccordionItemBody>
           </AccordionItem>
-          <AccordionItem>
+          <AccordionItem onClick={this.requestJobsData}>
             <AccordionItemTitle>
-              <h3>
-                {" "}
+              <h3 className="u-position-relative">
                 <FontAwesomeIcon icon={faClipboard} /> Jobs
+                <div className="accordion__arrow" role="presentation" />
               </h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              <p>Body content</p>
+              {this.state.jobsData.length === 0 && <p>No data</p>}
+              {this.state.jobsData.length > 0 && (
+                <ul className="list-group list-group-flush">
+                  {this.state.jobsData.map(comment => (
+                    <li className="list-group-item d-flex row">
+                      <div className="col-12">{comment.field_2173}</div>
+                      <div className="col-6">{comment.field_2190}</div>
+                      <div
+                        className="col-6"
+                        dangerouslySetInnerHTML={{ __html: comment.field_2196 }}
+                      >
+                        {}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {!this.state.jobsData && (
+                <div>
+                  <FontAwesomeIcon
+                    icon={faSpinner}
+                    size="2x"
+                    className="atd-spinner"
+                  />
+                </div>
+              )}
             </AccordionItemBody>
           </AccordionItem>
-          <AccordionItem>
+          <AccordionItem onClick={this.requestAttachmentsData}>
             <AccordionItemTitle>
-              <h3>
+              <h3 className="u-position-relative">
                 <FontAwesomeIcon icon={faPaperclip} /> Attachments
+                <div className="accordion__arrow" role="presentation" />
               </h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              <p>Body content</p>
+              {this.state.attachmentsData.length === 0 && <p>No data</p>}
+              {this.state.attachmentsData.length > 0 && (
+                <ul className="list-group list-group-flush">
+                  {this.state.attachmentsData.map(comment => (
+                    <li className="list-group-item d-flex row">
+                      <div className="col-12">{comment.field_2403}</div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {!this.state.attachmentsData && (
+                <div>
+                  <FontAwesomeIcon
+                    icon={faSpinner}
+                    size="2x"
+                    className="atd-spinner"
+                  />
+                </div>
+              )}
             </AccordionItemBody>
           </AccordionItem>
         </Accordion>

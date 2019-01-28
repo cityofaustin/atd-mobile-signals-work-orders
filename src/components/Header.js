78bed2127058899beb7683535992bb793966e37a
Mateo Clarke <code@mateoclarke.com>
2019-01-28 13:44:10 -0600

    refactor log out button event handler, don’t show…
    back button on home, and prettier autoformatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { Link, navigate, redirectTo } from '@reach/router';
-import Cookies from 'js-cookie';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import StyledHeader from '../styles/Header.css.js';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import StyledHeader from "../styles/Header.css.js";
 
 const Header = props => {
-
+  const isNotHomePath = props.location.pathname !== "/";
   return (
     <StyledHeader>
-      <button onClick={
-        () => window.history.go('-1')
-      }><FontAwesomeIcon icon={faArrowLeft} />
-      </button>
+      {isNotHomePath && (
+        <button onClick={() => props.history.goBack()}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+      )}
       <h2>{props.currentPage}</h2>
-      <button
-        onClick={() => {
-          Cookies.remove('knackUserToken');
-          navigate({ to: '/login' });
-        }}
-      >Log out</button>
-    </StyledHeader >
-  )
-}
+      <button onClick={props.revokeKnackUserToken}>Log out</button>
+    </StyledHeader>
+  );
+};
 
 export default Header;

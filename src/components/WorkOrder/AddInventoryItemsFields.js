1d7c2b127191ef2b85f9e7dc3128d64d7dbbd150
Mike Dilley <mike.dilley@gmail.com>
2019-07-17 11:31:04 -0500

    Add inventory items and item condition FIELDS in formConfig.js, add and style fields for inventory items and condition in AddInventoryItemsFields component

--- src/components/WorkOrder/AddInventoryItemsFields.js
+++ src/components/WorkOrder/AddInventoryItemsFields.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 
-import { INVENTORY_ITEMS_CONDITION_OPTIONS } from './formConfig';
+import { INVENTORY_ITEMS_CONDITION_OPTIONS, FIELDS } from './formConfig';
 import api from '../../queries/api';
 
 export default class AddInventoryItemsFields extends Component {
@@ -31,6 +31,46 @@ export default class AddInventoryItemsFields extends Component {
   };
 
   render() {
-    return <>Inventory Form</>;
+    return (
+      <>
+        <div className="form-group">
+          <label htmlFor={FIELDS.WORK_ORDER_INVENTORY_ITEMS}>
+            Inventory Item
+          </label>
+          <Select
+            className="basic-single"
+            classNamePrefix="select"
+            defaultValue={''}
+            isClearable
+            isSearchable
+            name={FIELDS.WORK_ORDER_INVENTORY_ITEMS}
+            options={this.state.itemOptions}
+            //   onChange={this.props.handleSupportTechniciansFieldChange.bind(
+            //     this,
+            //     FIELDS.SUPPORT_TECHNICIANS
+            //   )}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor={FIELDS.WORK_ORDER_CONDITION}>Condition</label>
+          <select
+            className="form-control"
+            id={FIELDS.WORK_ORDER_CONDITION}
+            name={FIELDS.WORK_ORDER_CONDITION}
+            defaultValue={''}
+          >
+            {INVENTORY_ITEMS_CONDITION_OPTIONS.map(option => (
+              <option value={option} key={option}>
+                {option}
+              </option>
+            ))}
+            {/*    onChange={this.props.handleSupportTechniciansFieldChange.bind(
+                 this,
+                 FIELDS.SUPPORT_TECHNICIANS
+               )} */}
+          </select>
+        </div>
+      </>
+    );
   }
 }

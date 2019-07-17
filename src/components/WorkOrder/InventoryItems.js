ede385e7f9c24be451d442c6e5941873615c226c
Mike Dilley <mike.dilley@gmail.com>
2019-07-17 12:01:25 -0500

    Add constructor and change handlers for items and condition to pass down to AddInventoryItemsFields in  InventoryItems

--- src/components/WorkOrder/InventoryItems.js
+++ src/components/WorkOrder/InventoryItems.js
@@ -5,9 +5,24 @@ import SubmitButton from '../Form/SubmitButton';
 import { faWrench } from '@fortawesome/free-solid-svg-icons';
 
 class InventoryItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {},
+      isSubmitting: false,
+    };
+  }
   componentDidMount() {
     console.log('In componentDidMount');
   }
+
+  handleInventoryItemChange = (item, field) => {
+    console.log(item, field);
+  };
+
+  handleItemConditionChange = (condition, field) => {
+    console.log(condition, field);
+  };
 
   submitForm = e => {
     e.preventDefault();
@@ -19,8 +34,11 @@ class InventoryItems extends Component {
       <div>
         <Header icon={faWrench} title="Add Inventory Items" />
         <form onSubmit={this.submitForm}>
-          <AddInventoryItemsFields />
-          <SubmitButton isSubmitting={false} />
+          <AddInventoryItemsFields
+            handleInventoryItemChange={this.handleInventoryItemChange}
+            handleItemConditionChange={this.handleItemConditionChange}
+          />
+          <SubmitButton isSubmitting={this.state.isSubmitting} />
         </form>
       </div>
     );

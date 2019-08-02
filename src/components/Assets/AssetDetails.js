3c071d81a6c88c24cdc5d958789fd126d39c9edd
Mike Dilley <mike.dilley@gmail.com>
2019-08-02 14:34:48 -0500

    Split data in AssetDetails.js into two columns

--- src/components/Assets/AssetDetails.js
+++ src/components/Assets/AssetDetails.js
@@ -5,22 +5,44 @@ import { FIELDS } from "./formConfig";
 const AssetDetails = props => {
   const details = props.assetDetails;
   const fields = FIELDS.ASSETS_DETAILS;
+
+  const firstColumnLength = Math.ceil(fields.details.length / 2);
   return (
     <div>
       <div className="row">
         <div className="col-6">
-          {fields.details.map(field => {
+          {fields.details.map((field, i) => {
+            console.log(firstColumnLength);
             const fieldId = Object.values(field)[0];
-            return (
-              <dl>
-                <dt>{changeCase.titleCase(Object.keys(field))}</dt>
-                <dd
-                  dangerouslySetInnerHTML={{
-                    __html: details[fieldId],
-                  }}
-                />
-              </dl>
-            );
+            if (i < firstColumnLength) {
+              return (
+                <dl>
+                  <dt>{changeCase.titleCase(Object.keys(field))}</dt>
+                  <dd
+                    dangerouslySetInnerHTML={{
+                      __html: details[fieldId],
+                    }}
+                  />
+                </dl>
+              );
+            }
+          })}
+        </div>
+        <div className="col-6">
+          {fields.details.map((field, i) => {
+            const fieldId = Object.values(field)[0];
+            if (i >= firstColumnLength) {
+              return (
+                <dl>
+                  <dt>{changeCase.titleCase(Object.keys(field))}</dt>
+                  <dd
+                    dangerouslySetInnerHTML={{
+                      __html: details[fieldId],
+                    }}
+                  />
+                </dl>
+              );
+            }
           })}
         </div>
       </div>

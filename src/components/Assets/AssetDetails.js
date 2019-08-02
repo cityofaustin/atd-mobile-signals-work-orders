ce1f8e7f2c70c2cbe59f4e8cddb43534ae39cb6a
Mike Dilley <mike.dilley@gmail.com>
2019-08-02 12:04:56 -0500

    Change data structure in formConfig.js to transform keys with changeCase library to titles, iterate over keys in AssetDetails

--- src/components/Assets/AssetDetails.js
+++ src/components/Assets/AssetDetails.js
@@ -1,12 +1,21 @@
 import React from "react";
+import changeCase from "change-case";
+import { FIELDS } from "./formConfig";
 
-const AssetDetails = props => (
-  <div>
-    <div className="row">
-      <div className="col-6">{props.assetDetails.field_199}</div>}
+const AssetDetails = props => {
+  const details = props.AssetDetails;
+  const fields = FIELDS.ASSETS_DETAILS;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-6">
+          {fields.details.map(field => (
+            <div>{changeCase.titleCase(Object.keys(field))}</div>
+          ))}
+        </div>
+      </div>
     </div>
-    }
-  </div>
-);
+  );
+};
 
 export default AssetDetails;
--- src/components/Assets/formConfig.js
+++ src/components/Assets/formConfig.js
@@ -1,37 +1,37 @@
 export const FIELDS = {
   ASSET_TYPE: "field_977",
   ASSETS_DETAILS: {
-    details: {
-      signalsId: "field_199",
-      control: "field_208",
-      secondarySignals: "field_1329", // HTML url, Link in Knack app
-      modifiedBy: "field_703_raw", // field_703 is HTML
-      modified: "field_205",
-      type: "field_201",
-      owner: "field_202",
-      funding: "field_1510",
-      bikeSignal: "field_1877",
-      status: "field_491",
-      turnOnDate: "field_204",
-      map: "field_210_raw", // Link in Knack app, field_210 is HTML <a>
-    },
-    components: {
-      pedestrianSignal: "field_1322",
-      firmware: "field_1563",
-      firmwareStatusDate: "field_1564",
-    },
-    locations: {
-      locationId: "field_209", // HTML url
-      signalEngineerArea: "field_188_raw", // field_188 is HTML
-      councilDistrict: "field_189_raw", // raw is an Array, field_189 is HTML
-      jurisdiction: "field_209.field_190_raw",
-    },
-    communications: {
-      commStatus: "field_1491",
-      commStatusDatetime: "field_1492",
-      cabinetType: "field_1788_raw", // field_1788 is HTML
-      UPS: "field_1785", // HTML, field_1785 is boolean
-    },
+    details: [
+      { signalsId: "field_199" },
+      { control: "field_208" },
+      { secondarySignals: "field_1329" }, // HTML url, Link in Knack app
+      { modifiedBy: "field_703_raw" }, // field_703 is HTML
+      { modified: "field_205" },
+      { type: "field_201" },
+      { owner: "field_202" },
+      { funding: "field_1510" },
+      { bikeSignal: "field_1877" },
+      { status: "field_491" },
+      { turnOnDate: "field_204" },
+      { map: "field_210_raw" }, // Link in Knack app, field_210 is HTML <a>
+    ],
+    components: [
+      { pedestrianSignal: "field_1322" },
+      { firmware: "field_1563" },
+      { firmwareStatusDate: "field_1564" },
+    ],
+    locations: [
+      { locationId: "field_209" }, // HTML url
+      { signalEngineerArea: "field_188_raw" }, // field_188 is HTML
+      { councilDistrict: "field_189_raw" }, // raw is an Array, field_189 is HTML
+      { jurisdiction: "field_209.field_190_raw" },
+    ],
+    communications: [
+      { commStatus: "field_1491" },
+      { commStatusDatetime: "field_1492" },
+      { cabinetType: "field_1788_raw" }, // field_1788 is HTML
+      { UPS: "field_1785" }, // HTML, field_1785 is boolean
+    ],
   },
   ASSETS_MAP: {
     name: "field_1058",

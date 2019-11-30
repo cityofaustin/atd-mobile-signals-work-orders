f070495d268847f632e3c2119e7e8a277efc398e
Mike Dilley <mike.dilley@gmail.com>
2019-11-30 08:29:47 -0600

    Uninstall Hotjar

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./styles/index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { hotjar } from "react-hotjar";
-
-// hotjar.initialize(1590618, 6);
 
 ReactDOM.render(
   <BrowserRouter>

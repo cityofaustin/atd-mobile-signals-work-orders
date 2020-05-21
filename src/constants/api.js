ea96330a5879ac9f733eb4922f0abe539b6d0a75
Mike Dilley <mike.dilley@gmail.com>
2020-05-21 11:08:41 -0500

    Remove app id comment

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const STAGING_APP_ID = "5e2216f0cbf8d9001616b034"; // 13-aug-2019--test-austin-transportation-data-tracker
+export const STAGING_APP_ID = "5e2216f0cbf8d9001616b034";
 export const PRODUCTION_APP_ID = "5815f29f7f7252cc2ca91c4f"; // amd
 export const APP_ID = process.env.REACT_APP_KNACK_APP_ID || STAGING_APP_ID;
